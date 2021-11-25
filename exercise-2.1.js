1e13384caaee282ed936e04849f098a1e0c9acb1
MahdiYosofzay <mahdiyosofzay@gmail.com>
2021-11-25 20:54:10 +0430

    first commit

---
+++ exercise-2.1.js
@@ -0,0 +1,26 @@
+/*
+ *   Exercise 2-1
+ *
+ *   Using only single-character output statements that output a hash mark,
+ *   a space, or an end-of-line symbol, write a program that outputs the
+ *   following shape:
+ *
+ *   ########
+ *    ######
+ *     ####
+ *      ##
+ */
+
+let shape = '';
+
+for (let i = 1; i <= 4; i++) {
+  for (let space = 1; space <= i - 1; space++) {
+    shape += ' ';
+  }
+  for (let j = 1; j <= 8 - 2 * (i - 1); j++) {
+    shape += '#';
+  }
+  shape += '\n';
+}
+
+console.log(shape);
